7d8297796cd0d9145f7c1e80c0ffd7536664e42e
swathisura <swathisura65@gmail.com>
2026-02-24 15:38:23 +0530

    Update variables.tf

--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -1,24 +1,7 @@
-variable "cluster_name" {
-  description = "Name of ECS cluster"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID where ECS service will run"
-  type        = string
-}
-
+variable "cluster_name" {}
+variable "ecr_image_url" {}
+variable "ecs_execution_role_arn" {}
+variable "vpc_id" {}
 variable "subnets" {
-  description = "List of subnet IDs for ECS tasks"
-  type        = list(string)
-}
-
-variable "ecs_execution_role_arn" {
-  description = "ARN of existing IAM role for ECS tasks"
-  type        = string
-}
-
-variable "ecr_image_url" {
-  description = "Full ECR image URL including tag"
-  type        = string
+  type = list(string)
 }
