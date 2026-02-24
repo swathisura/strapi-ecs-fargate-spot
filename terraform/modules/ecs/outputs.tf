9ff1481c9cdb522cb15ceab9220ddcad26ec47d4
swathisura <swathisura65@gmail.com>
2026-02-24 14:49:10 +0530

    Update outputs.tf

--- terraform/modules/ecs/outputs.tf
+++ terraform/modules/ecs/outputs.tf
@@ -1,3 +1,14 @@
-output "cluster_name" {
-  value = aws_ecs_cluster.this.name
+output "ecs_cluster_id" {
+  description = "ECS Cluster ID"
+  value       = aws_ecs_cluster.this.id
+}
+
+output "ecs_service_name" {
+  description = "ECS Service Name"
+  value       = aws_ecs_service.this.name
+}
+
+output "ecs_task_definition_arn" {
+  description = "Task Definition ARN"
+  value       = aws_ecs_task_definition.this.arn
 }
