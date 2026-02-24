2710dade6b508876b26ced634d124c264b139f9f
swathisura <swathisura65@gmail.com>
2026-02-24 15:35:07 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,22 +1,17 @@
-# === ECS Outputs ===
+# ECS outputs
 output "ecs_cluster_id" {
-  description = "ID of the ECS cluster"
-  value       = module.ecs.ecs_cluster_id
+  value = module.ecs.ecs_cluster_id
 }
 
 output "ecs_service_name" {
-  description = "Name of the ECS service"
-  value       = module.ecs.ecs_service_name
+  value = module.ecs.ecs_service_name
 }
 
 output "ecs_task_definition_arn" {
-  description = "ARN of the ECS task definition"
-  value       = module.ecs.ecs_task_definition_arn
+  value = module.ecs.ecs_task_definition_arn
 }
 
-# === Optional ECR Outputs (only if ECR module exists) ===
-# Uncomment this block if you have an ECR module called 'ecr'
-# output "ecr_repository_url" {
-#   description = "URL of the ECR repository"
-#   value       = module.ecr.repository_url
-# }
+# ECR output
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
+}
