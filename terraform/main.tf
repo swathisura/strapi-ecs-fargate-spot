35528bc8b8014a819c38d68d71be41f3aa6942d3
swathisura <swathisura65@gmail.com>
2026-02-24 13:07:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,26 @@ module "ecr" {
   repo_name   = var.ecr_repo_name
   common_tags = var.common_tags
 }
+module "ecr" {
+  source = "./modules/ecr"
+}
+
+# Use default VPC data
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+module "ecs" {
+  source       = "./modules/ecs"
+  cluster_name = "swathi-strapi"
+  image_url    = module.ecr.repository_url
+  vpc_id       = data.aws_vpc.default.id
+  subnets      = data.aws_subnets.default.ids
+}
