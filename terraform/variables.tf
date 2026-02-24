090773fa96e82c637eba5ff4ecbd9f5a262504b7
swathisura <swathisura65@gmail.com>
2026-02-24 15:33:59 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,25 +1,37 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "project_name" {
-  default = "swatho-strapi-platform"
-}
-
-variable "environment" {
-  default = "dev"
-}
-
 variable "ecr_repo_name" {
+  type    = string
   default = "swatho-strapi-app"
 }
 
 variable "common_tags" {
+  type = map(string)
   default = {
+    Environment = "dev"
+    ManagedBy   = "terraform"
+    Project     = "strapi-platform"
     Team        = "swatho"
     Owner       = "swatho-devops"
-    Project     = "strapi-platform"
-    ManagedBy   = "terraform"
-    Environment = "dev"
   }
+}
+
+variable "ecs_cluster_name" {
+  type    = string
+  default = "swathi-strapi"
+}
+
+variable "ecs_execution_role_arn" {
+  type        = string
+  description = "Use existing IAM role ARN for ECS task execution"
+}
+
+variable "subnets" {
+  type = list(string)
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "ecr_image_url" {
+  type = string
 }
