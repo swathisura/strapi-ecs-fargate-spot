89ffa9de644e2c17f2b94c3289c5b1dbbe71335b
swathisura <swathisura65@gmail.com>
2026-02-24 15:33:27 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "swatho-terraform-state"
-    key            = "strapi-platform/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket = "your-terraform-state-bucket"
+    key    = "strapi/terraform.tfstate"
+    region = "us-east-1"
   }
 }
