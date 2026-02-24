4082739f31570606e5354cebabd251ca3f297128
swathisura <swathisura65@gmail.com>
2026-02-24 15:12:59 +0530

    Update main.tf

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -58,8 +58,7 @@ resource "aws_ecs_service" "this" {
   cluster         = aws_ecs_cluster.this.id
   task_definition = aws_ecs_task_definition.this.arn
   desired_count   = 1
-  launch_type     = "FARGATE"
-
+  
   network_configuration {
     subnets          = var.subnets
     security_groups  = [aws_security_group.ecs.id]
